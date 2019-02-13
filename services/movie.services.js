cdf13a12e53319bb19f4210b6214b45905b137b3
Romina Manzano <romina.manzano@ecloudsolutions.com>
2019-02-13 11:54:34 -0300

    FIX: improve comments

--- services/movie.services.js
+++ services/movie.services.js
@@ -7,19 +7,27 @@ const db = low(adapter)
 const _ = require('underscore')
 
 exports.getSearch = (options, callback) => {
+  /*
+    Get the "search" term from the "options". If a search term exists, use it to get the
+    matching movies (with the provided mixin). If it doesn't exist, just get all the movies
+  */
 
-  // Return filtered movies by search term
+  /*
+    db._.mixin({
+      search: function(movies) {
+        return _.filter(movies, function(movie){ return movie.Title.toLowerCase().includes(search.toLowerCase());});
+      }
+    })
 
-  // db._.mixin({
-  //   search: function(movies) {
-  //     return _.filter(movies, function(movie){ return movie.Title.toLowerCase().includes(search.toLowerCase());});
-  //   }
-  // })
-
-  // Return all movies
-  // Handle error scenario
+    movies = db.get('movies').search().value()
+  */
 
   return;
 }
 
 // Get one movie by imdbID
+
+/*
+  Create a function "getOne" that receives an "id" and a "callback".
+  Find the movie by the given id and return it
+*/
